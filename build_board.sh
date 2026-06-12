5c1c4663302b6d1b1c0e92c6113a99771d508c1a
Per Sommer <persommer@mailbox.org>
2026-06-12 19:27:00 +0200

    framework update

--- build_board.sh
+++ build_board.sh
@@ -1,5 +1,9 @@
 # west build -s zmk/app -b nice_nano -d build/crisis_split_v1_left --pristine -- -DZMK_CONFIG="/home/ps/dev_github/zmk-config-2" -DSHIELD=crisis_split_v1_left && cp /home/ps/dev_github/zmk-config-2/build/crisis_split_v1_left/zephyr/zmk.uf2 /run/media/ps/NICENANO
 
-west build -s zmk/app -b nrfmicro_13 -d build/cosmos_5x6_with_trackballs_left --pristine -- -DZMK_CONFIG="/home/ps/dev_github/zmk-config-2" -DSHIELD=cosmos_5x6_with_trackballs_left && sleep 15&& cp /home/ps/dev_github/zmk-config-2/build/cosmos_5x6_with_trackballs_left/zephyr/zmk.uf2 /run/media/ps/NRF52BOOT/
+# west build -s zmk/app -b nrfmicro_13 -d build/cosmos_5x6_with_trackballs_left --pristine -- -DZMK_CONFIG="/home/ps/dev_github/zmk-config-2" -DSHIELD=cosmos_5x6_with_trackballs_left && sleep 15&& cp /home/ps/dev_github/zmk-config-2/build/cosmos_5x6_with_trackballs_left/zephyr/zmk.uf2 /run/media/ps/NRF52BOOT/
 
 # west build -s zmk/app -b nrfmicro_13 -d build/cosmos_5x6_with_trackballs_right --pristine -- -DZMK_CONFIG="/home/ps/dev_github/zmk-config-2" -DSHIELD=cosmos_5x6_with_trackballs_right && cp /home/ps/dev_github/zmk-config-2/build/cosmos_5x6_with_trackballs_right/zephyr/zmk.uf2 /run/media/ps/NRF52BOOT/
+
+
+# framework_v1
+# west build -s zmk/app -b nice_nano -d build/framework_v1 --pristine -- -DZMK_CONFIG="/home/ps/dev_github/zmk-config-2" -DSHIELD=framework_v1 && cp /home/ps/dev_github/zmk-config-2/build/framework_v1/zephyr/zmk.uf2 /run/media/ps/NICENANO
